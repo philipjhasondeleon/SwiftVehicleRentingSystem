1e995a53dd34161d9633259c9615bdcdcb79b1b0
philip <jhasondeleon@gmail.com>
2020-02-12 17:38:54 -0500

    [Philip] Person Class created

--- VehicleRentingSystem/Person.swift
+++ VehicleRentingSystem/Person.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+
+enum Gender
+{
+  case Male, Female, Other
+}
+class Person
+{
+   
+   
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var gender: Gender
+    var birthDate: Date?
+    var age: Int
+    {
+        return 0
+    }
+    var mobileNumber: Int
+    var emailId: String
+    var userName: String
+    var passWord: String
+    
+   
+  
+   
+    init(id:Int, firstName:String, lastName:String, gender:Gender, birthDate:Date, mobileNumber: Int, emailId: String, userName: String, passWord: String)
+    {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.gender = gender
+        self.birthDate = birthDate
+        self.mobileNumber = mobileNumber
+        self.emailId = emailId
+        self.userName = userName
+        self.passWord = passWord
+        
+    }
+}
