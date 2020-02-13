9faec4ac1a469fcd3899418fd7b5846b4c553f33
philip <jhasondeleon@gmail.com>
2020-02-12 20:08:52 -0500

    [Philip] Driver class Created

---
+++ VehicleRentingSystem/Driver.swift
@@ -0,0 +1,25 @@
+//
+//  Driver.swift
+//  VehicleRentingSystem
+//
+//  Created by MacStudent on 2020-02-12.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Driver : Person
+{
+    var drivingLicenseNo: String
+    var isDrivingHistoryCleared : Bool
+    var salary : Double
+
+    init(id: Int, firstName: String, lastName: String, gender: Gender, birthDate: Date, mobileNumber: Int, emailId: String, userName: String, passWord: String, drivingLicenseNo: String, isDrivingHistoryCleared : Bool, salary : Double)
+    {
+        self.drivingLicenseNo = drivingLicenseNo
+        self.isDrivingHistoryCleared = isDrivingHistoryCleared
+        self.salary = salary
+        super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, mobileNumber: mobileNumber, emailId: emailId, userName: userName, passWord: passWord)
+        
+    }
+}
