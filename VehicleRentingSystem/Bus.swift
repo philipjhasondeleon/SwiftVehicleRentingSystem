c510ad52a1d5dc1eddbcb80836846c126e6140dc
Philip <jhasondeleon@gmail.com>
2020-02-19 20:21:23 -0500

    [Philip] Motor class added

--- VehicleRentingSystem/Bus.swift
+++ VehicleRentingSystem/Bus.swift
@@ -10,11 +10,13 @@ import Foundation
 
 class Bus: Vehicle
 {
-var isAccessibilityServiceAvailble: Bool
-var isWifiAvailable: Bool
+    var typeOfBus: String
+    var isAccessibilityServiceAvailble: Bool
+    var isWifiAvailable: Bool
 
-    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: String, ratePerKilometer: String, isAccessibilityServiceAvailble: Bool, isWifiAvailable: Bool)
+    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: String, ratePerKilometer: String,typeOfBus: String, isAccessibilityServiceAvailble: Bool, isWifiAvailable: Bool)
     {
+        self.typeOfBus = typeOfBus
         self.isAccessibilityServiceAvailble = isAccessibilityServiceAvailble
         self.isWifiAvailable = isWifiAvailable
         super.init(vehicleId: vehicleId, vehicleDetails: vehicleDetails, manufacturerName: manufacturerName, vehicleIsSelfDrive: vehicleIsSelfDrive, vehicleDriverName: vehicleDriverName, vehicleIsInsured: vehicleIsInsured, insuranceProviderName: insuranceProviderName, noOfSeats: noOfSeats, fuelType: fuelType, baseRatePerDay: baseRatePerDay, ratePerKilometer: ratePerKilometer)
@@ -22,6 +24,7 @@ var isWifiAvailable: Bool
     override func display()
     {
         super.display()
+        print("Type of Bus: \(typeOfBus)")
         print("Accessibility Service Available: \(self.isAccessibilityServiceAvailble)")
         print("WifiAvailable: \(self.isWifiAvailable)")
     }
--- VehicleRentingSystem/Motor.swift
+++ VehicleRentingSystem/Motor.swift
@@ -13,10 +13,10 @@ class Motor: Vehicle
     var maxTopSpeed: Double
     var milage: Double
     
-    init(vehicleId: Int, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: String, ratePerKilometer: String, maxTopSpeed: Double, milage: Double)
+    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: String, ratePerKilometer: String, maxTopSpeed: Double, milage: Double)
     {
         self.maxTopSpeed = maxTopSpeed
         self.milage = milage
-        
+        super.init(vehicleId: vehicleId, vehicleDetails: vehicleDetails, manufacturerName: manufacturerName, vehicleIsSelfDrive: vehicleIsSelfDrive, vehicleDriverName: vehicleDriverName, vehicleIsInsured: vehicleIsInsured, insuranceProviderName: insuranceProviderName, noOfSeats: noOfSeats, fuelType: fuelType, baseRatePerDay: baseRatePerDay, ratePerKilometer: ratePerKilometer)
     }
 }
