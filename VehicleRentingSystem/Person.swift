0c1918d504add4a3a848a66ec74e8295968f76a9
anmariyafrancis <anmariya1915@gmail.com>
2020-02-19 21:34:16 -0500

    [anmariya] age function created

--- VehicleRentingSystem/Person.swift
+++ VehicleRentingSystem/Person.swift
@@ -16,17 +16,13 @@ class Person
     var firstName: String?
     var lastName: String?
     var gender: Gender?
-    var birthDate: Date?
-    var age: Int?
-    {
-        return 0
-    }
+    var birthDate: String?
     var mobileNumber: Int
     var emailId: String
     var userName: String
     var passWord: String
    
-    init(id:Int, firstName:String, lastName:String, gender:Gender, birthDate:Date, mobileNumber: Int, emailId: String, userName: String, passWord: String)
+    init(id:Int, firstName:String, lastName:String, gender:Gender, birthDate:String, mobileNumber: Int, emailId: String, userName: String, passWord: String)
     {
         self.id = id
         self.firstName = firstName
@@ -39,13 +35,21 @@ class Person
         self.passWord = passWord
         
     }
+    
+    func getAge(birthDate : String)->Int
+    {
+       let calcAge = calendar.components(.Year, fromDate: birthdayDate!, toDate: now, options: [])
+        let age = calcAge.year
+    }
+    {
+        
+    }
     func display()
        {
            print("Id            : \(self.id)")
            print("First Name    : \(self.firstName)")
            print("Last Name     : \(self.lastName)")
            print("Gender        : \(self.gender)")
-           print("BirthDate     : \(self.birthDate)")
            print("Age           : \(self.age)")
        }
 }
