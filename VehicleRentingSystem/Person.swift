ee08b8d0399d7b6a6002d8c8f434842a2766ed50
anmariyafrancis <anmariya1915@gmail.com>
2020-02-20 18:23:54 -0500

    [anmariya] changed the type of birthdate to date

--- VehicleRentingSystem/Person.swift
+++ VehicleRentingSystem/Person.swift
@@ -16,13 +16,13 @@ class Person
     var firstName: String?
     var lastName: String?
     var gender: Gender?
-    var birthDate: String?
+    var birthDate: Date?
     var mobileNumber: Int
     var emailId: String
     var userName: String
     var passWord: String
    
-    init(id:Int, firstName:String, lastName:String, gender:Gender, birthDate:String, mobileNumber: Int, emailId: String, userName: String, passWord: String)
+    init(id:Int, firstName:String, lastName:String, gender:Gender, birthDate:Date, mobileNumber: Int, emailId: String, userName: String, passWord: String)
     {
         self.id = id
         self.firstName = firstName
