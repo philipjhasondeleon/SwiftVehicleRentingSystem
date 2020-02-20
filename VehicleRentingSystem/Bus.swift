22f47d07de7b297acb8d4238d9d440fc97298c03
anmariyafrancis <anmariya1915@gmail.com>
2020-02-19 20:48:12 -0500

    [ANMARIYA] error removed in bus and motor class

--- VehicleRentingSystem/Bus.swift
+++ VehicleRentingSystem/Bus.swift
@@ -14,7 +14,7 @@ class Bus: Vehicle
     var isAccessibilityServiceAvailble: Bool
     var isWifiAvailable: Bool
 
-    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: String, ratePerKilometer: String,typeOfBus: String, isAccessibilityServiceAvailble: Bool, isWifiAvailable: Bool)
+    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: BaseRatePerDay, ratePerKilometer: RatePerKilometer,typeOfBus: String, isAccessibilityServiceAvailble: Bool, isWifiAvailable: Bool)
     {
         self.typeOfBus = typeOfBus
         self.isAccessibilityServiceAvailble = isAccessibilityServiceAvailble
--- VehicleRentingSystem/Motor.swift
+++ VehicleRentingSystem/Motor.swift
@@ -13,7 +13,7 @@ class Motor: Vehicle
     var maxTopSpeed: Double
     var milage: Double
     
-    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: String, ratePerKilometer: String, maxTopSpeed: Double, milage: Double)
+    init(vehicleId: String, vehicleDetails: String, manufacturerName: String, vehicleIsSelfDrive: String, vehicleDriverName: String, vehicleIsInsured: String, insuranceProviderName: String, noOfSeats: Int, fuelType: FuelType, baseRatePerDay: BaseRatePerDay, ratePerKilometer: RatePerKilometer, maxTopSpeed: Double, milage: Double)
     {
         self.maxTopSpeed = maxTopSpeed
         self.milage = milage
