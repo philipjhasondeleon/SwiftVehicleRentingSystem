0670773e975766452bf42111f71770de639edeae
anmariyafrancis <anmariya1915@gmail.com>
2020-02-19 21:02:54 -0500

    [anmariya] created dictionary to  add rented vehicle list

--- VehicleRentingSystem/Customer.swift
+++ VehicleRentingSystem/Customer.swift
@@ -12,9 +12,9 @@ class Customer: Person
 {
     var address: String
     var city: String
-    var vehicleRentList: String
+    var vehicleRentList: [String:Vehicle]
 
-    init(id: Int, firstName: String, lastName: String, gender: Gender, birthDate: Date, mobileNumber: Int, emailId: String, userName: String, passWord: String, address: String, city: String, vehicleRentList: String)
+    init(id: Int, firstName: String, lastName: String, gender: Gender, birthDate: Date, mobileNumber: Int, emailId: String, userName: String, passWord: String, address: String, city: String, vehicleRentList: [String:Vehicle])
     {
         self.address = address
         self.city = city
