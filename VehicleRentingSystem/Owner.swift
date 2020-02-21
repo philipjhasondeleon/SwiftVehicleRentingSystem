94b743450176244e170662f754e0a4dae9fa9a72
Philip <jhasondeleon@gmail.com>
2020-02-21 18:17:23 -0500

    [Philip] Owner Class created

---
+++ VehicleRentingSystem/Owner.swift
@@ -0,0 +1,34 @@
+//
+//  Owner.swift
+//  VehicleRentingSystem
+//
+//  Created by MacStudent on 2020-02-21.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Owner: Person
+{
+    var companyTitle : String
+    var businessLandLineNo : Int
+    var website : String
+    var vehicleListOwned = [String: Vehicle]()
+    
+    init(id: Int, firstName: String, lastName: String, gender: Gender, birthDate: String, mobileNumber: Int, emailId: String, userName: String, passWord: String, companyTitle : String, businessLandLineNo : Int, website : String)
+    {
+        self.companyTitle = companyTitle
+        self.businessLandLineNo = businessLandLineNo
+        self.website = website
+        super.init(id: id, firstName: firstName, lastName: lastName, gender: gender, birthDate: birthDate, mobileNumber: mobileNumber, emailId: emailId, userName: userName, passWord: passWord)
+    }
+    
+    override func display() {
+        super.display()
+        print("Company Title: \(self.companyTitle)")
+        print("LandLine No: \(self.businessLandLineNo)")
+        print("Website: \(self.website)")
+        print("Vehicle List Owned: \(self.vehicleListOwned)")
+    }
+}
+
