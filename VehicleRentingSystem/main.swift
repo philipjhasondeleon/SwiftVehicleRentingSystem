55a5e862fbed9946d9cc49549c71c29fcd229db1
anmariyafrancis <anmariya1915@gmail.com>
2020-02-20 21:06:22 -0500

    [anmariya] created 5 customer objects

--- VehicleRentingSystem/main.swift
+++ VehicleRentingSystem/main.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
-var c=Customer(id: 1, firstName: "Anmariya", lastName: "Francis", gender: .Male, birthDate: "1996-04-12", mobileNumber: 9876543210, emailId: "an@gmail.com", userName: "an", passWord: "11", address: "Scarborough", city: "Toronto")
+var c:Customer
+
+c=Customer(id: 1, firstName: "Anmariya", lastName: "Francis", gender: .Female, birthDate: "1996-04-12", mobileNumber: 9876543210, emailId: "an@gmail.com", userName: "an", passWord: "11", address: "Scarborough", city: "Toronto")
+c=Customer(id: 2, firstName: "Aldin", lastName: "Joseph", gender: .Male, birthDate: "1996-04-06", mobileNumber: 123456789, emailId: "al@gmail.com", userName: "al", passWord: "22", address: "Kennedy", city: "Toronto")
+c=Customer(id: 3, firstName: "Philip", lastName: "Jhason", gender: .Male, birthDate: "1997-03-02", mobileNumber: 9876543211, emailId: "ph@gmail.com", userName: "ph", passWord: "33", address: "Wilson", city: "Toronto")
+c=Customer(id: 4, firstName: "Jennifer", lastName: "Jiyo", gender: .Female, birthDate: "1970-03-18", mobileNumber: 9876543213, emailId: "je@gmail.com", userName: "je", passWord: "44", address: "Brimely", city: "Toronto")
+c=Customer(id: 5, firstName: "Jis", lastName: "Jiyo", gender: .Male, birthDate: "1980-10-26", mobileNumber: 9876543214, emailId: "jis2@gmail.com", userName: "jis", passWord: "55", address: "Miramer", city: "Toronto")
+
+
+
 
 
