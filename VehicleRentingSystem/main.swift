81dda703ceb3e79508023424a45af7e15d781b11
anmariyafrancis <anmariya1915@gmail.com>
2020-02-21 19:15:16 -0500

    [anmariya] created 5th customer

--- VehicleRentingSystem/main.swift
+++ VehicleRentingSystem/main.swift
@@ -11,6 +11,7 @@ import Foundation
 var c1=Customer(id: 1, firstName: "Anmariya", lastName: "Francis", gender: .Female, birthDate: Date.from(year: 1996, month: 4, day: 12), mobileNumber: 9876543210, emailId: "an@gmail.com", userName: "an", passWord: "aa", address: "Scarborough", city: "Toronto")
 var c2=Customer(id: 2, firstName: "Aldin", lastName: "Jose", gender: .Male, birthDate:Date.from(year: 1995, month: 4, day: 6), mobileNumber: 9876543211, emailId: "al@gmail.com", userName: "al", passWord: "al1", address: "Brimely", city: "Toronto")
 var c3=Customer(id: 3, firstName: "Philip", lastName: "Jhason", gender: .Male, birthDate: Date.from(year: 1997, month: 3, day: 2), mobileNumber: 9876543212, emailId: "ph@gmail.com", userName: "ph", passWord: "ph1", address: "Kenndey", city: "Toronto")
+var c4=Customer(id: 4, firstName: "Jennifer", lastName: "Jis", gender: .Male, birthDate: Date.from(year: 1986, month: 6, day: 9), mobileNumber: 987654323, emailId: "je@gmail.com", userName: "je", passWord: "je1", address: "Seminole", city: "Toronto")
 
 
 /* var customers=[Customer]()
